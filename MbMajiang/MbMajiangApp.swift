c0b4d5c0c1cd447d79e362e16ea5bcd5b0e43db1
dahlia1209 <dahlia1209@gmail.com>
2026-03-29 20:27:38 +0900

    Initial Commit

---
+++ MbMajiang/MbMajiangApp.swift
@@ -0,0 +1,17 @@
+//
+//  MbMajiangApp.swift
+//  MbMajiang
+//
+//  Created by Ryu Nakamura on 2026/03/29.
+//
+
+import SwiftUI
+
+@main
+struct MbMajiangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
